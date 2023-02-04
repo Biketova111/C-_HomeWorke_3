c03c1672fd8e21a2126fcc54e29fbbe6e7e5e158
Biketova111 <89241781930@mail.ru>
2023-02-04 19:38:10 +0900

    Add task3

---
+++ task3/Program.cs
@@ -0,0 +1,45 @@
+﻿/* Задача 23
+
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125 */
+
+int GetNumber (string message)
+{
+    int result = 0;
+    while (true)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out result) && result != 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Вы ввели не число или введенное число равно 0. Повторите ввод");
+        }
+    }
+    return result;
+}
+
+void GetSquare (int N)
+{
+    if (N>0)
+   {
+        for (int i = 1; i <= N; i++)
+        {
+            Console.Write($"{i*i*i}, ");
+        }
+    }
+    else 
+    {
+       for (int i = 1; i >= N; i--)
+        {
+            Console.Write($"{i*i*i}, ");
+        } 
+    }
+}
+
+int N = GetNumber("Введите число: ");
+GetSquare(N);
